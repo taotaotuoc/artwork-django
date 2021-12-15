cc36028b8695674037a4d02cb0e8860f7d0cb6d0
taotaotuoc <11542771@qq.com>
2021-12-15 10:10:19 -0500

    product and payments

---
+++ orders/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Payment,Order,OrderProduct
+
+
+class OrderProduceInline(admin.TabularInline):
+    model = OrderProduct
+    readonly_fields = ('payment', 'user', 'product', 'quantity', 'product_price', 'ordered')
+    extra = 0
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['order_number','full_name','phone','email','city','order_total','is_ordered','tax','status','created_at']
+    list_filter = ['status','is_ordered']
+    search_fields = ['order_number','first_name','last_name','phone','email']
+    list_per_page = 20
+    inlines = [ OrderProduceInline ]
+
+
+
+
+admin.site.register(Order,OrderAdmin)
+admin.site.register(Payment)
+admin.site.register(OrderProduct)
